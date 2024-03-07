946a497ae58981c16051f238f2267990679c2b2b
cedric.decoster <cedric.decoster@gmail.com>
2024-03-07 12:20:07 +0100

    Projektdateien hinzufügen.

---
+++ Ajuna.TestSuite/NodeTest.cs
@@ -0,0 +1,50 @@
+﻿using Substrate.NetApi.Model.Extrinsics;
+
+namespace Substrate.NetApi.TestNode
+{
+    public abstract class NodeTest
+    {
+        protected const string WebSocketUrl = "ws://127.0.0.1:9944";
+
+        protected SubstrateClient _substrateClient;
+
+
+        [SetUp]
+        public async Task ConnectAsync()
+        {
+            await _substrateClient.ConnectAsync();
+        }
+
+        [TearDown]
+        public async Task CloseAsync()
+        {
+            await _substrateClient.CloseAsync();
+        }
+
+        [OneTimeSetUp]
+        public void CreateClient()
+        {
+            _substrateClient = new SubstrateClient(new Uri(WebSocketUrl), ChargeTransactionPayment.Default());
+        }
+
+        [OneTimeTearDown]
+        public void DisposeClient()
+        {
+            _substrateClient.Dispose();
+        }
+
+        /// <summary>
+        /// Return the 20th hash block from now (totally arbitrary)
+        /// </summary>
+        /// <returns></returns>
+        protected async Task<byte[]> GivenBlockAsync()
+        {
+            var lastBlockData = await _substrateClient.Chain.GetBlockAsync();
+            var lastBlockNumber = lastBlockData.Block.Header.Number.Value;
+
+            var blockNumber = new Model.Types.Base.BlockNumber();
+            blockNumber.Create((uint)(lastBlockNumber - 20));
+            return (await _substrateClient.Chain.GetBlockHashAsync(blockNumber)).Bytes;
+        }
+    }
+}
